37cd75af909a8f27f6713bb020c56b1c1c259ade
HeinerKremer <heiner.kremer@tuebingen.mpg.de>
2020-05-27 14:24:49 +0200

    Added town settings for Bern

---
+++ sim/lib/settings/town_settings_bern.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+'''
+Settings for town generation
+'''
+
+town_name = 'Bern'
+
+# Make sure to download country-specific population density data
+# from https://data.humdata.org/organization/facebook
+population_path = 'lib/data/population/population_che_2019-07-01.csv'   # Population density file
+
+sites_path='lib/data/queries/'  # Directory containing OSM site query details
+bbox = (46.936785, 46.962799, 7.417316, 7.482204) # Coordinate bounding box
+
+# Population per age group in the region
+# Source: https://www.citypopulation.de/en/switzerland/admin/03__luzern/
+population_per_age_group = np.array([100744,     # 0-9
+                                     96337,     # 10-19
+                                     121374,     # 20-29
+                                     140048,     # 30-39
+                                     139161,     # 40-49
+                                     155954,     # 50-59
+                                     124107,     # 60-69
+                                     96542,     # 70-79
+                                     60710       # 80+
+                                     ])
+
+region_population = population_per_age_group.sum()
+town_population = 133791
+
+# Daily testing capacity per 100k people
+# Roughly 5k tests per day in Switzerland (rough average over time frame 10.03.-27.04.2020:
+# https://www.bag.admin.ch/bag/en/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html
+daily_tests_unscaled = int(5000 * town_population / 8570000)
+
+# Information about household structure (set to None if not available)
+# Source for Switzerland: https://www.bfs.admin.ch/bfs/de/home/statistiken/bevoelkerung/stand-entwicklung/haushalte.html
+household_info = {
+    'size_dist': [16, 29, 18, 23, 14],              # distribution of household sizes (1-5 people) in %
+    'soc_role': {
+        'children': [1, 8/10, 0, 0, 0, 0, 0, 0, 0],    # age groups 0,1 can be children
+        'parents': [0, 2/10, 1, 1, 1, 1, 0, 0, 0],     # age groups 1,2,3,4,5 can be parents
+        'elderly': [0, 0, 0, 0, 0, 0, 1, 1, 1]         # age groups 6,7,8 are elderly
+    }
+}
