overflow issue in town-generator.ipynb under np.int16 default fixed

sim/lib/settings/town_settings_heinsberg.py
@@ -21,13 +21,13 @@ population_per_age_group = np.array([
     55436,  # 15-34
     91858,  # 35-59
     55703,  # 60-79
-    15555]) # 80+
+    15555], dtype=np.int32)  # 80+
 
 town_population = 41673 # Population of the central town of the region
 region_population = population_per_age_group.sum()
 
 # Roughly 100k in total in Germany: https://www.rki.de/DE/Content/Infekt/EpidBull/Archiv/2020/Ausgaben/15_20.pdf?__blob=publicationFile
-daily_tests_unscaled = int(100000 * town_population / 83000000)
+daily_tests_unscaled = int(100000 * (town_population / 83000000))
 
 # Information about household structure (set to None if not available)
 # Source for Germany: https://www.destatis.de/EN/Themes/Society-Environment/Population/Households-Families/Tables/lrbev05.html 

sim/lib/settings/town_settings_jura.py
@@ -25,14 +25,14 @@ population_per_age_group = np.array([7291,     # 0-9
                                      8986,     # 60-69
                                      6650,     # 70-79
                                      4400       # 80+
-                                     ])
+                                     ], dtype=np.int32)
 
 region_population = population_per_age_group.sum()
 town_population = region_population     # Consider full region in simulation
 
 # Roughly 5k tests per day in Switzerland (rough average over time frame 10.03.-27.04.2020:
 # https://www.bag.admin.ch/bag/en/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html
-daily_tests_unscaled = int(5000 * town_population / 8570000)
+daily_tests_unscaled = int(5000 * (town_population / 8570000))
 
 # Information about household structure (set to None if not available)
 # Source for Switzerland: https://www.bfs.admin.ch/bfs/de/home/statistiken/bevoelkerung/stand-entwicklung/haushalte.html

sim/lib/settings/town_settings_locarno.py
@@ -24,14 +24,14 @@ population_per_age_group = np.array([30078,     # 0-9
                                      41660,     # 60-69
                                      35925,     # 70-79
                                      24171      # 80+
-                                     ])
+                                     ], dtype=np.int32)
 
 region_population = population_per_age_group.sum()
 town_population = 15824
 
 # Roughly 5k tests per day in Switzerland (rough average over time frame 10.03.-27.04.2020:
 # https://www.bag.admin.ch/bag/en/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html
-daily_tests_unscaled = int(5000 * town_population / 8570000)
+daily_tests_unscaled = int(5000 * (town_population / 8570000))
 
 # Information about household structure (set to None if not available)
 # Source for Switzerland: https://www.bfs.admin.ch/bfs/de/home/statistiken/bevoelkerung/stand-entwicklung/haushalte.html

sim/lib/settings/town_settings_tirschenreuth.py
@@ -23,7 +23,7 @@ population_per_age_group = np.array([
     25655,  # 35-59
     16323,  # 60-79
     5831    # 80+
-    ])
+    ], dtype=np.int32)
 
 
 region_population = population_per_age_group.sum()
@@ -31,7 +31,7 @@ town_population = region_population
 
 
 # Roughly 100k in total in Germany: https://www.rki.de/DE/Content/Infekt/EpidBull/Archiv/2020/Ausgaben/15_20.pdf?__blob=publicationFile
-daily_tests_unscaled = int(100000 * town_population / 83000000)
+daily_tests_unscaled = int(100000 * (town_population / 83000000))
 
 # Information about household structure (set to None if not available)
 # Source for Germany: https://www.destatis.de/EN/Themes/Society-Environment/Population/Households-Families/Tables/lrbev05.html 