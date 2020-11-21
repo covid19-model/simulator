fbcdea42f8c98e3b121d6644fb28e378fb99961d
HeinerKremer <heiner.kremer@tuebingen.mpg.de>
2020-11-21 22:03:40 +0100

    Prepared experiments for test run

--- sim/sim-beacon-cache.py
+++ sim/sim-beacon-cache.py
@@ -1,5 +1,5 @@
-
 import sys, os
+
 if '..' not in sys.path:
     sys.path.append('..')
 
@@ -11,14 +11,15 @@ import multiprocessing
 import argparse
 from lib.measures import *
 from lib.experiment import Experiment, options_to_str, process_command_line
-from lib.calibrationSettings import calibration_lockdown_dates, calibration_mob_paths, calibration_states, contact_tracing_adoption
+from lib.calibrationSettings import calibration_lockdown_dates, calibration_mob_paths, calibration_states, \
+    contact_tracing_adoption
 from lib.calibrationFunctions import get_calibrated_params
 
 TO_HOURS = 24.0
 
 if __name__ == '__main__':
 
-    name = 'beacon-baseline'
+    name = 'beacon-cache'
     start_date = '2021-01-01'
     end_date = '2021-04-01'
     random_repeats = 48
@@ -27,10 +28,15 @@ if __name__ == '__main__':
     seed_summary_path = None
     set_initial_seeds_to = {}
     expected_daily_base_expo_per100k = 5 / 7
+    beacon_config = dict(
+        mode='all',
+    )
 
     # contact tracing experiment parameters
-    # ps_adoption = [1.0, 0.75, 0.65, 0.5]
-    ps_adoption = [1.0, 0.75, 0.5, 0.25]
+    p_adoption = 0.5
+    p_recall = 1.0
+    beacon_caches = [1.0, 0.5, 0]
+    beacons_only = True
 
     # seed
     c = 0
@@ -50,13 +56,18 @@ if __name__ == '__main__':
     calibrated_params = get_calibrated_params(country=country, area=area,
                                               multi_beta_calibration=False,
                                               maxiters=maxBOiters)
+
     # for debugging purposes
     if args.smoke_test:
         start_date = '2021-01-01'
-        end_date = '2021-01-05'
-        random_repeats = 2
+        # end_date = '2021-01-15'
+        random_repeats = 1
         full_scale = False
         ps_adoption = [0.5]
+        ps_recall = [1.0]
+        beacon_config = dict(
+            mode='all',
+        )
 
     # create experiment object
     experiment_info = f'{name}-{country}-{area}'
@@ -71,19 +82,24 @@ if __name__ == '__main__':
     )
 
     # contact tracing experiment for various options
-    for p_adoption in ps_adoption:
-
+    for beacon_cache in beacon_caches:
         # measures
         max_days = (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days
 
         m = [
+            # beacon measures
+            ManualTracingForAllMeasure(
+                t_window=Interval(0.0, TO_HOURS * max_days),
+                p_participate=.5,
+                p_recall=p_recall),
+
             # standard tracing measures
             ComplianceForAllMeasure(
                 t_window=Interval(0.0, TO_HOURS * max_days),
                 p_compliance=p_adoption),
             SocialDistancingForSmartTracing(
-                t_window=Interval(0.0, TO_HOURS * max_days), 
-                p_stay_home=1.0, 
+                t_window=Interval(0.0, TO_HOURS * max_days),
+                p_stay_home=1.0,
                 smart_tracing_isolation_duration=TO_HOURS * 14.0),
             SocialDistancingForSmartTracingHousehold(
                 t_window=Interval(0.0, TO_HOURS * max_days),
@@ -97,7 +113,8 @@ if __name__ == '__main__':
                 t_window=Interval(0.0, TO_HOURS * max_days),
                 p_isolate=1.0,
                 smart_tracing_isolation_duration=TO_HOURS * 14.0),
-            ]
+        ]
+
 
         # set testing params via update function of standard testing parameters
         def test_update(d):
@@ -115,22 +132,25 @@ if __name__ == '__main__':
             d['smart_tracing_tested_contacts'] = 100000
 
             # if true only contacts at sites with beacons can be traced
-            d['beacons_only'] = False
+            d['beacons_only'] = beacons_only
             # Visits of i `beacon_cache` hours before and after visits of j get tracked
-            d['beacon_cache'] = 0
+            d['beacon_cache'] = beacon_cache
 
             return d
 
+
         simulation_info = options_to_str(
             p_adoption=p_adoption,
+            p_recall=p_recall,
+            beacon_cache=beacon_cache
         )
-            
+
         experiment.add(
             simulation_info=simulation_info,
             country=country,
             area=area,
             measure_list=m,
-            beacon_config=None, 
+            beacon_config=beacon_config,
             test_update=test_update,
             seed_summary_path=seed_summary_path,
             set_initial_seeds_to=set_initial_seeds_to,
@@ -138,7 +158,7 @@ if __name__ == '__main__':
             full_scale=full_scale,
             lockdown_measures_active=False,
             expected_daily_base_expo_per100k=expected_daily_base_expo_per100k)
-                
+
     print(f'{experiment_info} configuration done.')
 
     # execute all simulations
