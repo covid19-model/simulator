c4ea3a1393988af18186231eb2d366a4fc3fcd21
Lars Lorch <lars.lorch@gmail.com>
2021-01-29 13:13:06 +0100

    Updated region mobility settings

--- sim/lib/settings/town_settings_bern.py
+++ sim/lib/settings/town_settings_bern.py
@@ -5,6 +5,7 @@ Settings for town generation
 '''
 
 town_name = 'Bern'
+country = 'CH'
 
 # Make sure to download country-specific population density data
 # from https://data.humdata.org/organization/facebook
